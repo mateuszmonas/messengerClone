e5c2570b98552457e48dcd4720b4a4b95297da30
mateuszmonas <mateuszmonas@gmail.com>
2019-03-16 14:41:47 +0100

    flatView+input

--- App.js
+++ App.js
@@ -7,25 +7,60 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, FlatList, TextInput} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+type Props = {
+  age: number;
+  name: string;
+};
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
+type State = {
+  data: [];
+};
+
+export default class App extends Component<Props, State> {
+
+
+  constructor(props: P, context: any) {
+    super(props, context);
+
+    this.state = {
+      data: [{username: 'dsa', id: '1'}, {username: 'dsa', id: '2'}]
+    }
+  }
+
+  render() {                console.log('asdsda');
+
+      return (
+          <View>
+              <FlatList
+                  data={this.state.data}
+                  renderItem={({item}) =>
+                      <View>
+                          <Text>{item.username}</Text>
+                      </View>
+                  }
+                  keyExtractor={(item, index) => item.id}
+              />
+              <TextInput
+                  onSubmitEditing={(event) => {
+                      const text = event.nativeEvent.text;
+                      console.log(text);
+                      this.setState(state => {
+                          let id = state.data.slice(-1)[0].id;
+                          const data = state.data.concat([{
+                              username: text,
+                              id: (+id + 1).toString()
+                          }]);
+                          return {
+                              data
+                          };
+                      });
+                  }}
+                  style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+              />
+          </View>
+      );
   }
 }
 
@@ -35,15 +70,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    color: "red"
   },
 });
