a4d7b43bd2fc8bfd9a68fb0f27d72f9e3aff0844
mateuszmonas <mateuszmonas@gmail.com>
2019-03-18 19:14:45 +0100

    ui upgrade

--- LoginScreen.js
+++ LoginScreen.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {AsyncStorage, Button, TextInput, View} from 'react-native';
+import {AsyncStorage, Button, StyleSheet, TextInput, View} from 'react-native';
 import WebController from "./web/WebController";
 
 type State = {
@@ -32,18 +32,43 @@ export class LoginScreen extends Component<Props, State>{
     }
     render(): React.ReactNode {
         return (
-            <View>
-                <TextInput style={{ borderColor: '#000' , borderBottomWidth:1}}
-                    onChangeText={(text) => this.setState({loginText: {text}})}
+            <View style={styles.container}>
+                <TextInput style={styles.usernameInput}
+                           onChangeText={(text) => this.setState({loginText: {text}})}
                 />
-                <TextInput secureTextEntry={true}
-                    onChangeText={(text) => this.setState({passwordText: {text}})}
+                <TextInput style={styles.passwordInput} secureTextEntry={true}
+                           onChangeText={(text) => this.setState({passwordText: {text}})}
                 />
-                <Button title='LOGIN'
-                        onPress={() => this._onLoginClick()}/>
-                <Button title='REGISTER'
-                        onPress={() => this._onRegisterClick()}/>
+                <View style={styles.bottom}>
+                    <Button style={styles.loginButton} title='LOGIN'
+                            onPress={() => this._onLoginClick()}/>
+                    <Button style={styles.registerButton} title='REGISTER'
+                            onPress={() => this._onRegisterClick()}/>
+                </View>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: '100%',
+        height: '100%',
+        backgroundColor: '#48d2f0'
+    },
+    usernameInput: {
+        backgroundColor: '#fff',
+        borderColor: '#000',
+        borderBottomWidth: 1
+    },
+    passwordInput: {
+        backgroundColor: '#fff',
+    },
+    loginButton: {},
+    registerButton: {},
+    bottom: {
+        flex: 1,
+        justifyContent: 'flex-end',
+    }
+});
